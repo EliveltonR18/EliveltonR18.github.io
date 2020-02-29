5377d8da87534b1929675cefaeb236c55b4b95db
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2020-02-29 03:28:56 -0300

    [develop] | fix deploy

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Thumbnail, Text } from './styles';
-import { Header } from '../components/theme';
+// import { Header } from '../components/theme';
 import dev from 'assets/illustrations/not_found.svg'
 
 export default () => (
   <Layout>
-    <Header />
     <SEO title="404: Not found" location="/404" />
+    {/* <Header /> */}
     <Thumbnail>
       <img src={dev} alt="Sou Elivelton Rodrigues, um Software Engineer" />
     </Thumbnail>
