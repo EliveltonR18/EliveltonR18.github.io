b9b0223a7be88875cff9d3e8fa15f018aa7e610d
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2020-02-29 03:25:24 -0300

    [develop] | added custom page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
+import { Thumbnail, Text } from './styles';
+import { Header } from '../components/theme';
+import dev from 'assets/illustrations/not_found.svg'
 
 export default () => (
   <Layout>
+    <Header />
     <SEO title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Thumbnail>
+      <img src={dev} alt="Sou Elivelton Rodrigues, um Software Engineer" />
+    </Thumbnail>
+    <Text>
+      <span>Página não encontrada!</span>
+    </Text>
   </Layout>
 );
---
+++ src/pages/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Thumbnail = styled.div`
+  flex: 1;
+  @media (max-width: 960px) {
+    padding-top: 50px;
+    padding-bottom: 50px;
+    width: 100%;
+  }
+
+  img {
+    width: 50%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+export const Text = styled.div`
+  flex: 1;
+
+  @media (max-width: 960px) {
+      font-size: 20pt;
+      color: #707070;
+      width: 40%;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+  }
+
+  span {
+    font-size: 36pt;
+    color: #cc6b6b;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding-top: 100px;
+  }
+`;
