5747382319aa1e5c1db16a7fcb49be8a8ed5fd3f
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2020-02-29 03:58:10 -0300

    [develop] | change image for page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
-import { Thumbnail, Text } from './_styles';
+import { Thumbnail } from './_styles';
 import { Header } from '../components/theme';
 import dev from 'assets/illustrations/not_found.svg'
 
 export default () => (
   <Layout>
-    <SEO title="404: Not found" location="/404" />
+    <SEO title="404 - Not found" location="/404" />
     <Header />
     <Thumbnail>
       <img src={dev} alt="Error 404" />
     </Thumbnail>
-    <Text>
-      <span>Página não encontrada!</span>
-    </Text>
   </Layout>
 );
